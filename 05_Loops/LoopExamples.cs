0005195dd6ce42d9adf8bb231ba90450d0ecc8ef
AndroidDoctorr <gbartthenerd@gmail.com>
2021-12-09 15:15:47 -0500

    added loops

---
+++ 05_Loops/LoopExamples.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _05_Loops
+{
+    [TestClass]
+    public class LoopExamples
+    {
+        [TestMethod]
+        public void WhileLoops()
+        {
+            int total = 0;
+
+            while (total < 10)
+            {
+                // ++ before means it adds 1 before using the number
+                // ++ after means it adds 1 after using it
+                Console.WriteLine(++total);
+                // total = total + 1;
+                // total += 2;
+                // total++;
+            }
+
+            total = 0;
+            while (true)
+            {
+                if (total++ == 10)
+                {
+                    Console.WriteLine("Goal reached!");
+                    break;
+                }
+            }
+
+            Random rng = new Random();
+            int someNumber;
+            bool keepLooping = true;
+
+            while (keepLooping)
+            {
+                someNumber = rng.Next(0, 20);
+
+                if (someNumber == 6 || someNumber == 10)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(someNumber);
+
+                if (someNumber == 15)
+                {
+                    keepLooping = false;
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ForLoops()
+        {
+            int studentCount = 16;
+            // 1 - Initial action - happens once at the start
+            // 2 - Loop Condition - keep looping while this == true
+            // 3 - Iterative action - happens at the end of every loop
+            // 4 - The Loop - the code that is repeated
+            //        1          2             3
+            for (int i = 0; i < studentCount; i++)
+            {
+                // 4
+                Console.WriteLine(i);
+            }
+
+            Random rng = new Random();
+            for (int i = 0; i != 10; i = rng.Next(0, 20))
+            {
+                Console.WriteLine(i);
+            }
+
+            string[] students = { "Jonah", "Andrew L", "Andrew S", "Brandon", "Doug" };
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+
+            // specific to collections/IEnumerable
+            foreach (string student in students)
+            {
+                Console.WriteLine($"Welcome to class, {student}!");
+            }
+
+            foreach (char letter in "Andrew Torr")
+            {
+                Console.WriteLine(letter);
+            }
+        }
+
+
+        [TestMethod]
+        public void DoWhileLoops()
+        {
+            int iterator = 0;
+            do
+            {
+                Console.WriteLine("Hello!");
+                iterator++;
+            } while (iterator < 5);
+
+            do
+            {
+                Console.WriteLine("this should only happen once");
+            } while (false);
+
+            while (false)
+            {
+                Console.WriteLine("This should never happen!");
+            }
+        }
+    }
+}
---
+++ 05_Loops/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("05_Loops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("05_Loops")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4cb92be-4f64-4a58-88dc-89e5d715d263")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
