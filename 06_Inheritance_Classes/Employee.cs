fcffe4a20a5218952a14b0a1a253a61629ab2506
AndroidDoctorr <gbartthenerd@gmail.com>
2021-12-10 21:23:41 -0500

    inheritance classes pt 1

---
+++ 06_Inheritance_Classes/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Inheritance_Classes
+{
+    public class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+
+    public class HourlyEmployee : Employee
+    {
+        public decimal HourlyWage { get; set; }
+        public double HoursWorked { get; set; }
+    }
+
+    public class SalaryEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+    }
+}
---
+++ 07_Inheritance_Tests/PersonTests.cs
@@ -0,0 +1,37 @@
+﻿using _06_Inheritance_Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _07_Inheritance_Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void NameTest()
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Name);
+            // person.SetFirstName("Terry");
+            person.SetLastName("Brown");
+            Console.WriteLine(person.Name);
+        }
+
+        [TestMethod]
+        public void EmployeeTest()
+        {
+            Employee employee = new Employee();
+            employee.Email = "something@somewhere.com";
+            employee.SetFirstName("Job");
+            employee.SetLastName("Worker");
+            employee.HireDate = new DateTime(2020, 01, 01);
+            Console.WriteLine($"{employee.Name} was hired on {employee.HireDate}");
+
+            HourlyEmployee hourlyEmployee = new HourlyEmployee();
+            hourlyEmployee.Email = "sadfgd";
+            hourlyEmployee.SetFirstName("sdsfgd");
+            hourlyEmployee.HireDate = DateTime.Now;
+            hourlyEmployee.HourlyWage = 30;
+        }
+    }
+}
