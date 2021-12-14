f9df866cd30d5eb51bfaff401c55d59038eafbdc
AndroidDoctorr <gbartthenerd@gmail.com>
2021-12-14 14:07:00 -0500

    added solutions to the repo challenge with movies and shows

--- 09_StreamingContent_Inheritance/StreamingRepository.cs
+++ 09_StreamingContent_Inheritance/StreamingRepository.cs
@@ -46,7 +46,32 @@ namespace _09_StreamingContent_Inheritance
         // CHALLENGE:
 
         // GetMoviesByRating()
+        public List<Movie> GetMoviesByRating(MaturityRating rating)
+        {
+            // V1
+            return (List<Movie>) _contentDirectory
+                .Where(c => c.MaturityRating == rating && c is Movie)
+                .Select(c => (Movie) c);
+
+
+            // V2
+            List<Movie> movies = new List<Movie>();
+            foreach(StreamingContent content in _contentDirectory)
+            {
+                if (content.MaturityRating == rating && content is Movie)
+                {
+                    movies.Add((Movie) content);
+                }
+            }
+            return movies;
+        }
 
         // GetFamilyFriendlyShows()
+        public List<Show> GetFamilyFriendlyShows()
+        {
+            return (List<Show>) _contentDirectory.Where(sc => sc is Show && sc.IsFamilyFriendly);
+
+            return GetAllShows().Where(s => s.IsFamilyFriendly).ToList();
+        }
     }
 }
